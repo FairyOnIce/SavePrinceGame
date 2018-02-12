5ed896cb791b481e6ca11a772294ee597b1b2ef8
Yumi Kondo <yumik091186@gmail.com>
2018-02-11 21:37:01 -0800

    RGB color defenition

---
+++ color.py
@@ -0,0 +1,8 @@
+white = (255, 255, 255)  ## RGB
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 155, 0)
+magenta = (255, 0, 255)
+purple = (75, 0, 130)
+princess_skin = (255, 159, 0)
+pcince_skin = (253,241,203)
